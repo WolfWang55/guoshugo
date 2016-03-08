1aa64231a344c584ae79c445f056fe17d698bce5
WolfWang55 <thelast_wolf@163.com>
2016-03-08 17:38:09 +0800

    Initial Commit

---
+++ guoshugo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  guoshugo
+//
+//  Created by LG on 16/3/8.
+//  Copyright (c) 2016年 LG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ guoshugo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  guoshugo
+//
+//  Created by LG on 16/3/8.
+//  Copyright (c) 2016年 LG. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
